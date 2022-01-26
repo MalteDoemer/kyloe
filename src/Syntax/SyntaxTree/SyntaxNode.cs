0c590b5e18e92744faed175cc87d512904cb8dcd
Malte Dömer <maltedoemer@gmail.com>
2022-01-26 10:47:36 +0100

    Syntax: Implemented ToString() on SyntaxNode

--- src/Syntax/SyntaxTree/SyntaxNode.cs
+++ src/Syntax/SyntaxTree/SyntaxNode.cs
@@ -1,6 +1,17 @@
+using System.IO;
+
 namespace Kyloe.Syntax
 {
     abstract class SyntaxNode
     {
+        public override string ToString()
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                var prettyWriter = new PrettyWriter(writer);
+                prettyWriter.Write(this);
+                return writer.ToString();
+            }
+        }
     }
 }
