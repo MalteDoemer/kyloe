d4b40c903d1eabfd079bd987224daaf3488a16c4
Malte Dömer <maltedoemer@gmail.com>
2022-02-26 16:56:22 +0100

    Namespace: Added Name member

--- kyloe/src/SemanticAnalysis/Namespace.cs
+++ kyloe/src/SemanticAnalysis/Namespace.cs
@@ -5,17 +5,21 @@ namespace Kyloe.Semantics
 {
     public class Namespace
     {
-        public Namespace(ImmutableDictionary<string, Namespace> namespaces, ImmutableDictionary<string, TypeReference> types)
+        public Namespace(string name, ImmutableDictionary<string, Namespace> namespaces, ImmutableDictionary<string, TypeReference> types)
         {
+            Name = name;
             Namespaces = namespaces;
             Types = types;
         }
 
+        public string Name { get; }
         public ImmutableDictionary<string, Namespace> Namespaces { get; }
         public ImmutableDictionary<string, TypeReference> Types { get; }
 
         public TypeReference? GetTypeOrNull(string name) => Types.GetValueOrDefault(name);
 
         public Namespace? GetNamespaceOrNull(string name) => Namespaces.GetValueOrDefault(name);
+
+        public override string? ToString() => Name;
     }
 }
