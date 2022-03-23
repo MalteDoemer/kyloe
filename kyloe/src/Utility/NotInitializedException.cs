2601936ee61dee5c5c1315aa2c9b9de1e41787e2
Malte Dömer <maltedoemer@gmail.com>
2022-03-23 08:57:35 +0100

    Utility: Removed NotInitializedExcpetion

--- kyloe/src/Utility/NotInitializedException.cs
+++
@@ -1,8 +0,0 @@
-namespace Kyloe.Utility
-{
-    public class NotInitializedException : System.Exception
-    {
-        public NotInitializedException(string field) : base($"The field '{field}' is not initialized") { }
-        public NotInitializedException(string field, string message) : base($"The field '{field}' is not initialized: {message}") { }
-    }
-}
