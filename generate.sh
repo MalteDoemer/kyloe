d7af0c6a96f05a3de8e2af997a130435fb6c77d4
Malte Dömer <maltedoemer@gmail.com>
2022-04-30 10:02:09 +0200

    Added bash script to generate parser

---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+EXE="$ROOT_DIR/grammar/bin/Debug/net6.0/grammar"
+
+GRAMMAR_DEF="$ROOT_DIR/kyloe/src/SyntaxAnalysis/grammar.def"
+GENERATOR_JSON="$ROOT_DIR/kyloe/src/SyntaxAnalysis/generator.json"
+OUT_DIR="$ROOT_DIR/kyloe/src/SyntaxAnalysis/Generated/"
+
+
+"$EXE" "$GRAMMAR_DEF" generate "$GENERATOR_JSON" "$OUT_DIR"
