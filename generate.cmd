abc1b1ad5c4ce1be9d470245a1d3b12c154c0f11
Malte Dömer <maltedoemer@gmail.com>
2022-04-30 10:01:18 +0200

    Fixed small issue in batch script

--- generate.cmd
+++ generate.cmd
@@ -7,6 +7,6 @@ SET "EXE=%ROOT_DIR%grammar\bin\Debug\net6.0\grammar.exe"
 
 SET "GRAMMAR_DEF=%ROOT_DIR%kyloe\src\SyntaxAnalysis\grammar.def"
 SET "GENERATOR_JSON=%ROOT_DIR%kyloe\src\SyntaxAnalysis\generator.json"
-SET "OUT_DIR=%ROOT_DIR%kyloe\src\SyntaxAnalysis\Generated\"
+SET "OUT_DIR=%ROOT_DIR%kyloe\src\SyntaxAnalysis\Generated"
 
 "%EXE%" "%GRAMMAR_DEF%" generate "%GENERATOR_JSON%" "%OUT_DIR%"
